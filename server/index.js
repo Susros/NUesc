1a569ec114e8996470295e7e83bea11a0222e4a1
Susros <kelvin.susros@gmail.com>
2019-10-31 04:36:04 +1100

    Added server

---
+++ server/index.js
@@ -0,0 +1,80 @@
+/**
+ * NUesc Server
+ * 
+ * This server handles request from Raspberry Pi when
+ * the sound event is detected.
+ * 
+ * @author Kelvin Yin
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+// Set up environment
+const dotenv       = require('dotenv');
+dotenv.config();
+
+// Set up express framework
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const helmet       = require('helmet');
+const cookieparser = require('cookie-parser');
+const socket       = require('socket.io');
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieparser());
+
+// Listen
+const server = app.listen(process.env.PORT);
+
+// Web Socket
+var ws = socket(server);
+
+// Check web socket connection
+ws.on('connection', (s) => {
+    console.log("Socket connection received: " + s.id);
+});
+
+// Sound Class
+const soundClass = [
+    'Air Conditioner',      
+    'Car Horn',             
+    'Children Playing',     
+    'Dog Bark',             
+    'Drilling',             
+    'Engine Idling',        
+    'Gun Shot',             
+    'Jackhammer',           
+    'Siren',                
+    'Street Music'          
+]
+
+app.post('/detected', (req, res) => {
+
+    const soundId = req.body.sound_id;
+
+    if (soundId < 0 || soundId >= soundClass.length) {
+        res.status(400).json({ message: 'Invalid sound ID.' });
+    } else {
+
+        ws.sockets.emit('detected', {
+            data: {
+                id: soundId,
+                event: soundClass[soundId]
+            }
+        });
+
+        res.status(200).json({ 
+            data: {
+                id: soundId,
+                event: soundClass[soundId]
+            }
+        });
+    }
+});
+
+app.get('/soundclass', (req, res) => {
+    res.status(200).json({ data: soundClass });
+});
