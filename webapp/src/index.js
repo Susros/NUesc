bf9008d3d4b9ebce76d14f41ccaddb531b069d57
Susros <kelvin.susros@gmail.com>
2019-10-31 19:22:45 +1100

    Added Front End

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import './sass/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
